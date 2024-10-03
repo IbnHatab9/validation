3219e96ec8742950c4dc5909c520ba8fe9bcf7e6
Ridial Faye <ridialbayefall33@gmail.com>
2024-10-03 08:57:39 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// document.getElementById('menu-toggle').addEventListener('click', function() {
+//     // On sélectionne la liste du menu
+//     var menuList = document.getElementById('menu-list');
+    
+//     // On ajoute ou enlève la classe 'active' pour afficher/masquer la liste
+//     if (menuList.classList.contains('active')) {
+//         menuList.classList.remove('active');
+//     } else {
+//         menuList.classList.add('active');
+//     }
+// });
